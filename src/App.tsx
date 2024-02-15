7591eb4cda21a14525e79d981091a3097128cbb9
Vlad Pashko <vladpavloggg@gmail.com>
2024-02-15 19:29:57 +0200

    added linter, prettier, husky and stage lint

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,6 @@ type MyComponentProps = {
   name: string;
 };
 
-const num = null;
-
-if (num === undefined) {
-  // eslint-disable-next-line no-console
-  console.log('123');
-}
-
 const MyComponent: React.FC<MyComponentProps> = ({ name }) => {
   return <div>Hello, {name}!</div>;
 };
