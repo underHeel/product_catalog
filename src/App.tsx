f153c37fa303a8bfa37acc36cacbe11016f5b58e
Alyona Nabok <alyonana42@gmail.com>
2024-02-15 23:00:45 +0200

    add rules for prettier, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BurgerIcon } from './components/ui/icons/BurgerIcon';
 
 const MyComponent: React.FC = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      Hello, world!
+      <BurgerIcon />
+    </div>
+  );
 };
 
 export default MyComponent;
