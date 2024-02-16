ce506a2a5bc1a11ede8b5df9b5f3e03cfb4be2c2
Yevhen <steplerjeka@gmail.com>
2024-02-16 22:51:40 +0200

    some footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
+import { Footer } from './components/Footer';
+import { ArrowUpIcon } from './components/ui/icons/ArrowUpIcon';
 
 const App: React.FC = () => {
   return (
     <div>
       <NavBar />
+      <Footer />
+      <ArrowUpIcon />
       <Outlet />
     </div>
   );
