e809cf364e1cc637e0c316162c2c9c296711e455
White EdenI <danil.gayvoronsky2201@gmail.com>
2024-02-16 22:53:54 +0200

    Created NotFoundPage component

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <div className="main">
+        <div className="main-numbers">404</div>
+        <div className="main-letters">Page not found. Try again later</div>
+      </div>
+    </div>
+  );
+};
