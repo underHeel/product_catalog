952ca32322086da773dbf826dc4095bbfc806338
Alyona Nabok <alyonana42@gmail.com>
2024-02-17 16:54:43 +0200

    add button components, change styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { NavBar } from './components/NavBar/NavBar';
 import './styles/utils/main.scss';
 import { IconButton } from './components/ui/buttons/IconButton';
 import { ArrowUpIcon } from './components/ui/icons/ArrowUpIcon';
+import { Button } from './components/ui/buttons/Button';
+import { ArrowLeftIconBlack } from './components/ui/icons/ ArrowLeftIconBlack';
 
 const App: React.FC = () => {
   return (
@@ -12,6 +14,10 @@ const App: React.FC = () => {
       <IconButton onClick={() => {}}>
         <ArrowUpIcon />
       </IconButton>
+      <Button onClick={() => {}} variant="text">
+        <ArrowLeftIconBlack />
+        Button
+      </Button>
       <Outlet />
     </div>
   );
