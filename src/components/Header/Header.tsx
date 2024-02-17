42efa835a65872f8db1fb8c46dbb5eee22dfe88e
Nazarii <kostrubitskiiwork@gmail.com>
2024-02-17 20:37:26 +0200

    create header without burger, add navbar component, delete margins from navbar, update hover mixin, add scale on Logo(test)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,12 @@
-import classNames from 'classnames';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Logo from '../../../public/images/Logo.png';
 import { BurgerIcon } from '../ui/icons/BurgerIcon';
 import { CartIcon } from '../ui/icons/CartIcon';
 import { FavoriteIcon } from '../ui/icons/FavoriteIcon';
+import { NavBar } from '../NavBar/NavBar';
 
 export const Header: React.FC = () => {
-  const isActiveLink = ({ isActive }: { isActive: boolean }) =>
-    classNames(`${styles.nav__link}`, {
-      [styles.nav__border]: isActive,
-    });
-
   return (
     <>
       <header className={styles.header}>
@@ -19,33 +14,7 @@ export const Header: React.FC = () => {
           <img src={Logo} alt="header_logo" className={styles.logo} />
         </Link>
 
-        <nav className={styles.nav}>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <NavLink to="/" className={isActiveLink}>
-                HOME
-              </NavLink>
-            </li>
-
-            <li className={styles.item}>
-              <NavLink to="/phones" className={isActiveLink}>
-                PHONES
-              </NavLink>
-            </li>
-
-            <li className={styles.item}>
-              <NavLink to="/tablets" className={isActiveLink}>
-                TABLETS
-              </NavLink>
-            </li>
-
-            <li className={styles.item}>
-              <NavLink to="/accessories" className={isActiveLink}>
-                ACCESSORIES
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+        <NavBar />
 
         <div className={styles.header__icons}>
           <Link to="/" className={`${styles.container} ${styles.burger}`}>
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.scss';
+
+export const NavBar: React.FC = () => {
+  const isActiveLink = ({ isActive }: { isActive: boolean }) =>
+    classNames(`${styles.nav__link}`, {
+      [styles.nav__border]: isActive,
+    });
+
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <NavLink to="/" className={isActiveLink}>
+            HOME
+          </NavLink>
+        </li>
+
+        <li className={styles.item}>
+          <NavLink to="/phones" className={isActiveLink}>
+            PHONES
+          </NavLink>
+        </li>
+
+        <li className={styles.item}>
+          <NavLink to="/tablets" className={isActiveLink}>
+            TABLETS
+          </NavLink>
+        </li>
+
+        <li className={styles.item}>
+          <NavLink to="/accessories" className={isActiveLink}>
+            ACCESSORIES
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
