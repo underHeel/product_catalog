4d0169e01a0ec646f09d08faad8dda2bdc4a168b
Yevhen <steplerjeka@gmail.com>
2024-02-18 13:30:42 +0200

    Footer task_8693ur8kw

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
 import './styles/utils/main.scss';
 import { Footer } from './components/Footer';
-import { Button } from './components/ui/buttons/Button';
-import { ArrowLeftIconBlack } from './components/ui/icons/ ArrowLeftIconBlack';
 
 const App: React.FC = () => {
   return (
     <div>
       <NavBar />
-      <IconButton onClick={() => {}}>
-        <ArrowUpIcon />
-      </IconButton>
-      <Outlet />
+      <Footer />
     </div>
   );
 };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,40 @@
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
-import NiceGadgetsLogo from '../../images/nice-gadgets.png';
-import NiceGadgetsOk from '../../images/nice-gadgets-ok.png';
 import { IconButton } from '../ui/buttons/IconButton';
 import { ArrowUpIcon } from '../ui/icons/ArrowUpIcon';
+import Logo from '../../../public/images/Logo.png';
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footer__left}>
-        <img src={NiceGadgetsLogo} alt="Logo" className={styles.footer__logo} />
-        <img src={NiceGadgetsOk} alt="LogoOk" className={styles.footer__logo} />
-      </div>
-      <div className={styles.footer__center}>
-        <a href="https://github.com" className={styles.footer__link}>
-          GITHUB
-        </a>
-        <a href="/contacts" className={styles.footer__link}>
-          CONTACTS
-        </a>
-        <a href="/contacts" className={styles.footer__link}>
-          RIGHTS
-        </a>
-      </div>
-      <div className={styles.footer__button}>
-        <a href="/" className={styles.footer__link}>
-          Back to top
-        </a>
-        <div className={styles.footer__arrow}>
-          <IconButton onClick={() => {}}>
-            <ArrowUpIcon />
-          </IconButton>
+    <div className={styles.topBorder}>
+      <footer className={styles.footer}>
+        <div className={styles.logo}>
+          <Link to="/" className={styles.link}>
+            <img src={Logo} alt="header_logo" />
+          </Link>
         </div>
-      </div>
-    </footer>
+        <div className={styles.additional}>
+          <a href="https://github.com" className={styles.additionalLink}>
+            GITHUB
+          </a>
+          <a href="/contacts" className={styles.additionalLink}>
+            CONTACTS
+          </a>
+          <a href="/contacts" className={styles.additionalLink}>
+            RIGHTS
+          </a>
+        </div>
+        <div className={styles.goTopButton}>
+          <div className={styles.button}>
+            <Link to="/" className={styles.buttonTop}>
+              Back to top
+            </Link>
+            <IconButton onClick={() => {}}>
+              <ArrowUpIcon />
+            </IconButton>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
