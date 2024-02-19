741259a340993b63788b3d70b09e2dd29d6b1a14
Vlad Pashko <vladpavloggg@gmail.com>
2024-02-19 16:46:49 +0200

    render all phones cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import './styles/utils/main.scss';
+import { GetPagination } from './api/getPhones';
 
 const App: React.FC = () => {
   return (
     <div>
+      <GetPagination />
       <Header />
       <Outlet />
     </div>
