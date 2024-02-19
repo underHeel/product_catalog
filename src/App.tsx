39a5b2b97bd0aa874f42e648a841d679cd2de131
Alyona Nabok <alyonana42@gmail.com>
2024-02-19 11:54:19 +0200

    add images, fix pathes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import './styles/utils/main.scss';
-import { Phone } from './types/Phone';
-
-import * as phonesAPI from './api/phones';
 
 const App: React.FC = () => {
-  const [phones, setPhones] = useState<Phone[]>([]);
-
-  useEffect(() => {
-    phonesAPI.getPhones(1, 10).then(setPhones);
-  }, []);
-
   return (
     <div>
       <Header />
       <Outlet />
-      {phones.map(({ name }) => (
-        <p key={name}>{name}</p>
-      ))}
     </div>
   );
 };
