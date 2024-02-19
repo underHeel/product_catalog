5763c63b925a9d636a5d0d3f5e154bd8b7a840ff
Yevhen <steplerjeka@gmail.com>
2024-02-19 15:21:33 +0200

    Added CartItemFolder

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './styles/utils/main.scss';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
