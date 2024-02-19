83e26d5e9b352ecc28e2585290173fda768d4d29
Yevhen <steplerjeka@gmail.com>
2024-02-20 00:05:50 +0200

    added cartItem with mobile landing

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,37 +1,59 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react/button-has-type */
 import { useState } from 'react';
 import { Phone } from 'src/types/Phone';
+import styles from './CartItem.module.scss';
 
 type Props = {
   phone: Phone;
 };
 
 export const CartItem: React.FC<Props> = ({ phone }) => {
-  const { id, name, priceRegular, images } = phone;
+  const { name, priceRegular, images } = phone;
   const [quantity, setQuantity] = useState<number>(1);
 
   return (
-    <div>
-      {id}
-      <div className="cart-item">
-        <button className="remove-button">x</button>
-        <img src={images[1]} alt="header_logo" />
-        <div className="product-details">
-          <h3 className="product-name">{name}</h3>
-          <div className="quantity-control">
-            <button className="decrement-button">-</button>
-            <span className="quantity">{quantity}</span>
-            <button
-              className="increment-button"
-              onClick={() => {
-                setQuantity((prev) => prev + 1);
-              }}
-            >
-              +
-            </button>
-          </div>
-          <span className="price">{`$${priceRegular}`}</span>
+    <div className={styles.cart}>
+      <div className={styles.cart_name}>
+        <div className={styles.cart_item}>
+          <button className={styles.remove_button}>x</button>
+          <img
+            src={images[1]}
+            alt={`${name} photo`}
+            className={styles.product_image}
+          />
         </div>
+        <div className={styles.product_details}>
+          <h3 className={styles.product_name}>{name}</h3>
+        </div>
+      </div>
+      <div className={styles.quantity_control}>
+        <div className={styles.quantity}>
+          <button
+            className={styles.on_button}
+            onClick={() => {
+              setQuantity((prev) => {
+                if (prev === 1) {
+                  return 1;
+                }
+
+                return prev - 1;
+              });
+            }}
+          >
+            -
+          </button>
+          <span className={styles.quantity}>{quantity}</span>
+          <button
+            className={styles.on_button}
+            onClick={() => {
+              setQuantity((prev) => prev + 1);
+            }}
+          >
+            +
+          </button>
+        </div>
+        <span className={styles.price}>{`$${priceRegular * quantity}`}</span>
       </div>
     </div>
   );
