5ab1fcf62d7606cbb2a65d520eb735bdbaa13a51
Yevhen <steplerjeka@gmail.com>
2024-02-19 21:04:28 +0200

    cartItem before merge with main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './styles/utils/main.scss';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import * as api from './api/phones';
+import { Phone } from './types/Phone';
 
 const App: React.FC = () => {
+  const [phones, setPhones] = useState<Phone[]>([]);
+  const [phone, setPhone] = useState<Phone>({} as Phone);
+
+  function loadPhones() {
+    api
+      .getPhones()
+      .then((data) => {
+        setPhones(data);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        console.log('Finished');
+        console.log(phones);
+        setPhone(phones[54]);
+      });
+  }
+
+  useEffect(loadPhones, []);
+
+  console.log(phone?.name);
+
   return (
     <div>
       <Header />
