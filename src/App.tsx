55f23e36cc42641459936d862c12b62e0355e117
White EdenI <danil.gayvoronsky2201@gmail.com>
2024-02-20 14:54:01 +0200

    Added mixins

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 
 import './styles/utils/main.scss';
 
 const App: React.FC = () => {
-<<<<<<<<< Temporary merge branch 1
-=========
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [phones, setPhones] = useState<Phone[]>([]);
-
-  useEffect(() => {
-    phonesAPI.getPhones(1, 10).then(setPhones);
-  }, []);
-
->>>>>>>>> Temporary merge branch 2
   return (
     <div>
       <Header />
       <Outlet />
-      {phones.map(({ name }) => (
-        <p key={name}>{name}</p>
-      ))}
     </div>
   );
 };
