4ae9ef309873c6d4331ee62984f8954e36f0e57f
Alyona Nabok <alyonana42@gmail.com>
2024-02-23 19:56:45 +0200

    add dark logo on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../redux/hooks';
 import styles from './Footer.module.scss';
 import { IconButton } from '../ui/buttons/IconButton';
 import { ArrowUpIcon } from '../ui/icons/ArrowUpIcon';
 import Logo from '/img/Logo.png';
+import LogoDark from '/img/LogoDark.png';
 
 export const Footer = () => {
+  const { theme } = useAppSelector((state) => state.theme);
+
   return (
     <div className={styles.footerWrapper}>
       <footer className={styles.footer}>
         <div className={styles.logo}>
           <Link to="/" className={styles.link}>
-            <img src={Logo} alt="header_logo" className={styles.image} />
+            <img
+              src={theme === 'light' ? Logo : LogoDark}
+              alt="header_logo"
+              className={styles.image}
+            />
           </Link>
         </div>
         <div className={styles.additional}>
