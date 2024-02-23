64b89c69aa5aa406ce5df3a5dba2421da89e79e4
Nazarii <kostrubitskiiwork@gmail.com>
2024-02-23 14:52:00 +0200

    fix category section, change scale

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -11,44 +11,50 @@ export const Category: React.FC = () => {
       <h2 className={styles.title}>Shop by category</h2>
       <div className={styles.container}>
         <Link to="/phones?page=1">
-          <div className={styles.inner}>
-            <img
-              src={phonesCategory}
-              alt="phones category"
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.cardTitle}>
-            <p className={styles.categoryName}>Mobile phones</p>
-            <p className={styles.categoryLength}>95 models</p>
+          <div className={styles.box}>
+            <div className={styles.inner}>
+              <img
+                src={phonesCategory}
+                alt="phones category"
+                className={styles.image}
+              />
+            </div>
+            <div className={styles.cardTitle}>
+              <p className={styles.categoryName}>Mobile phones</p>
+              <p className={styles.categoryLength}>95 models</p>
+            </div>
           </div>
         </Link>
 
         <Link to="/tablets">
-          <div className={styles.inner}>
-            <img
-              src={tabletCategory}
-              alt="tablet category"
-              className={styles.image}
-            />
-          </div>
-          <div>
-            <p className={styles.categoryName}>Tablets</p>
-            <p className={styles.categoryLength}>24 models</p>
+          <div className={styles.box}>
+            <div className={styles.inner}>
+              <img
+                src={tabletCategory}
+                alt="tablet category"
+                className={styles.image}
+              />
+            </div>
+            <div>
+              <p className={styles.categoryName}>Tablets</p>
+              <p className={styles.categoryLength}>24 models</p>
+            </div>
           </div>
         </Link>
 
         <Link to="/accessories">
-          <div className={styles.inner}>
-            <img
-              src={accessoriesCategory}
-              alt="accessories category"
-              className={styles.image}
-            />
-          </div>
-          <div>
-            <p className={styles.categoryName}>Accessories</p>
-            <p className={styles.categoryLength}>100 models</p>
+          <div className={styles.box}>
+            <div className={styles.inner}>
+              <img
+                src={accessoriesCategory}
+                alt="accessories category"
+                className={styles.image}
+              />
+            </div>
+            <div>
+              <p className={styles.categoryName}>Accessories</p>
+              <p className={styles.categoryLength}>100 models</p>
+            </div>
           </div>
         </Link>
       </div>
