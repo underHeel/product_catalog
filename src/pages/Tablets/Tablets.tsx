804252b88116bebe4f46b6ff3df7ea59bd18c8c7
Alyona Nabok <alyonana42@gmail.com>
2024-02-24 02:58:00 +0200

    add appropriate phones to sliders on home page, add fixes

--- src/pages/Tablets/Tablets.tsx
+++ src/pages/Tablets/Tablets.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ErrorComponent } from '../../components/ErrorComponent/ErrorComponent';
-import noProductImg from '../../../public/img/no_product.png';
+import noProductImg from '/img/no_product.png';
 
 export const Tablets: React.FC = () => {
   return (
