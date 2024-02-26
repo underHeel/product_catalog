bd95624ffcf93d65ff4242cba1e873117657f98f
Alyona Nabok <alyonana42@gmail.com>
2024-02-26 20:38:11 +0200

    add adding to cart and favorite functionality

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -15,22 +15,22 @@ import styles from './ProductPage.module.scss';
 export const ProductPage: React.FC = () => {
   const [product, setProduct] = useState<DetailedProduct | null>(null);
   const [category, setCategory] = useState<Category | null>(null);
+  const [allProducts, setAllProducts] = useState<Product[]>([]);
   const [suggestedProducts, setSuggestedProducts] = useState<Product[]>([]);
   const { state, pathname } = useLocation();
 
   const itemId = pathname.substring(pathname.lastIndexOf('/') + 1);
 
   useEffect(() => {
+    getAllProducts().then(setAllProducts);
     if (state) {
       setCategory(state.data);
     } else {
-      getAllProducts().then((products) => {
-        const foundCategory = products.find(
-          (item) => item.itemId === itemId,
-        )?.category;
+      const foundCategory = allProducts.find(
+        (item) => item.itemId === itemId,
+      )?.category;
 
-        setCategory(foundCategory as Category);
-      });
+      setCategory(foundCategory as Category);
     }
 
     if (category) {
@@ -38,13 +38,13 @@ export const ProductPage: React.FC = () => {
     }
 
     getPhone(itemId).then(setProduct);
-  }, [category, itemId, state]);
+  }, [category, itemId, state, allProducts]);
 
   return (
     <>
       {product && (
         <div className={styles.wrapper}>
-          <ItemOptions product={product} />
+          <ItemOptions product={product} allProducts={allProducts} />
           <div className={styles.container}>
             <ItemAbout product={product} />
             <ItemSpech product={product} />
