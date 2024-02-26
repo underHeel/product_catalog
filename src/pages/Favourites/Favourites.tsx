4292d7b134658793eee3310f5feb39cd2c2b9847
Vlad Pashko <vladpavloggg@gmail.com>
2024-02-26 03:59:05 +0200

    added empty favourites page design

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ErrorComponent } from '../../components/ErrorComponent/ErrorComponent';
 import { useAppSelector } from '../../redux/hooks';
 import { ProductCard } from '../../components/ProductCard';
 import styles from './Favourites.module.scss';
+import noProductImg from '../../../public/img/add_product.png';
 
 export const Favourites: React.FC = () => {
   const { favoritesList } = useAppSelector((state) => state.favorites);
@@ -11,6 +13,12 @@ export const Favourites: React.FC = () => {
       <div className={styles.favoritesWrapper}>
         <h1 className={styles.title}>Favourites</h1>
         <p className={styles.subTitle}>{`${favoritesList.length} items`}</p>
+        {favoritesList.length === 0 && (
+          <ErrorComponent
+            image={noProductImg}
+            errorMessage="No favourite items yet"
+          />
+        )}
         <div className={styles.list}>
           {favoritesList.map((product) => (
             <ProductCard key={product.id} product={product} />
