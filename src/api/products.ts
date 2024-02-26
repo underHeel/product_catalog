f1c5d15542a9a9de7bd049b5002c637aae2a19ce
Alyona Nabok <alyonana42@gmail.com>
2024-02-26 21:37:37 +0200

    add fixes

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,13 +1,16 @@
-/* eslint-disable indent */
 import { DetailedProduct } from 'src/types/DetailedProduct';
 import { Category } from '../types/Category';
 import { sortProducts } from '../services/sortProducts';
 import { Product } from '../types/Product';
 
 const API_URL_PRODUCTS = `https://underheel.github.io/product_catalog/products.json`;
-const API_URL_PHONES = `https://underheel.github.io/product_catalog/phones.json`;
-const API_URL_TABLETS = `https://underheel.github.io/product_catalog/tablets.json`;
-const API_URL_ACCESSORIES = `https://underheel.github.io/product_catalog/accessories.json`;
+const API_BASE_URL = `https://underheel.github.io/product_catalog`;
+
+const API_URLS: Record<Category, string> = {
+  [Category.phones]: `${API_BASE_URL}/phones.json`,
+  [Category.tablets]: `${API_BASE_URL}/tablets.json`,
+  [Category.accessories]: `${API_BASE_URL}/accessories.json`,
+};
 
 export const getAllProducts = (): Promise<Product[]> =>
   fetch(API_URL_PRODUCTS).then((response) => {
@@ -20,13 +23,12 @@ export const getAllProducts = (): Promise<Product[]> =>
 
 export const getAllProductsByCategory = (
   category: string,
-): Promise<Product[]> => {
-  return fetch(API_URL_PRODUCTS)
+): Promise<Product[]> =>
+  fetch(API_URL_PRODUCTS)
     .then((response) => response.json())
     .then((products) =>
       products.filter((product: Product) => product.category === category),
     );
-};
 
 export const getProducts = (
   category: Category,
@@ -37,29 +39,19 @@ export const getProducts = (
   const end = perPage * page;
   const start = end - perPage;
 
-  return getAllProductsByCategory(category).then((products) => {
-    return sortProducts(sortBy, products).slice(start, end);
-  });
+  return getAllProductsByCategory(category).then((products) =>
+    sortProducts(sortBy, products).slice(start, end),
+  );
 };
 
 export const getProduct = (
   category: Category,
   itemId: string,
 ): Promise<DetailedProduct> => {
-  let apiUrl: string;
+  const apiUrl = API_URLS[category];
 
-  switch (category) {
-    case Category.phones:
-      apiUrl = API_URL_PHONES;
-      break;
-    case Category.tablets:
-      apiUrl = API_URL_TABLETS;
-      break;
-    case Category.accessories:
-      apiUrl = API_URL_ACCESSORIES;
-      break;
-    default:
-      throw new Error('Invalid category');
+  if (!apiUrl) {
+    throw new Error('Invalid category');
   }
 
   return fetch(apiUrl)
