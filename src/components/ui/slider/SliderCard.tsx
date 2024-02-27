3da99e66bf2271dc7619b7ffbc4303ec542c11e6
Vlad Pashko <vladpavloggg@gmail.com>
2024-02-27 13:30:56 +0200

    fixed slider skeleton

--- src/components/ui/slider/SliderCard.tsx
+++ src/components/ui/slider/SliderCard.tsx
@@ -42,34 +42,42 @@ export const SliderCard: React.FC<Props> = ({ title, items, id }) => {
           </div>
         </div>
       </div>
-
-      <Swiper
-        navigation={{
-          nextEl: `.next-${id}`,
-          prevEl: `.prev-${id}`,
-          disabledClass: styles.disabled,
-        }}
-        loop
-        modules={[Navigation]}
-        spaceBetween={16}
-        breakpoints={{
-          0: {
-            slidesPerView: 'auto',
-          },
-          640: {
-            slidesPerView: 'auto',
-          },
-          1200: {
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {items.map((item) => (
-          <SwiperSlide key={item.id} className={styles.slider}>
-            {loading ? <CardSkeleton /> : <ProductCard product={item} />}
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className={styles.swippeWrapper}>
+        <Swiper
+          navigation={{
+            nextEl: `.next-${id}`,
+            prevEl: `.prev-${id}`,
+            disabledClass: styles.disabled,
+          }}
+          loop
+          modules={[Navigation]}
+          spaceBetween={16}
+          breakpoints={{
+            0: {
+              slidesPerView: 'auto',
+            },
+            640: {
+              slidesPerView: 'auto',
+            },
+            1200: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {loading &&
+            [...Array(10)].map(() => (
+              <SwiperSlide className={styles.slider}>
+                <CardSkeleton />
+              </SwiperSlide>
+            ))}
+          {!loading &&
+            items.map((item) => (
+              <SwiperSlide key={item.id} className={styles.slider}>
+                <ProductCard product={item} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
