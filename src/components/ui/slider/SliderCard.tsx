9eb9b5360d473c31edc8dd4079d1763349246c41
Alyona Nabok <alyonana42@gmail.com>
2024-02-27 12:36:49 +0200

    fix styles for sliders on home page

--- src/components/ui/slider/SliderCard.tsx
+++ src/components/ui/slider/SliderCard.tsx
@@ -37,34 +37,35 @@ export const SliderCard: React.FC<Props> = ({ title, items, id }) => {
           </div>
         </div>
       </div>
-
-      <Swiper
-        navigation={{
-          nextEl: `.next-${id}`,
-          prevEl: `.prev-${id}`,
-          disabledClass: styles.disabled,
-        }}
-        loop
-        modules={[Navigation]}
-        spaceBetween={16}
-        breakpoints={{
-          0: {
-            slidesPerView: 'auto',
-          },
-          640: {
-            slidesPerView: 'auto',
-          },
-          1200: {
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {items.map((item) => (
-          <SwiperSlide key={item.id} className={styles.slider}>
-            <ProductCard product={item} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className={styles.swippeWrapper}>
+        <Swiper
+          navigation={{
+            nextEl: `.next-${id}`,
+            prevEl: `.prev-${id}`,
+            disabledClass: styles.disabled,
+          }}
+          loop
+          modules={[Navigation]}
+          spaceBetween={16}
+          breakpoints={{
+            0: {
+              slidesPerView: 'auto',
+            },
+            640: {
+              slidesPerView: 'auto',
+            },
+            1200: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {items.map((item) => (
+            <SwiperSlide key={item.id} className={styles.slider}>
+              <ProductCard product={item} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
